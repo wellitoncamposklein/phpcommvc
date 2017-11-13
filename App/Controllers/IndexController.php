6fb814968f748a411d567cc72604dd81e1780b1b
Welliton Klein <welliton.fokushima@gmail.com>
2017-11-12 23:22:10 -0200

    Controllers

---
+++ App/Controllers/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+class IndexController
+{
+
+  public function index(){
+    echo "Route / Controller: index";
+  }
+
+  public function contact(){
+    echo "Route /contact Controller: contact";
+  }
+
+}
+ ?>
--- App/Route.php
+++ App/Route.php
@@ -8,12 +8,30 @@ class Route
 
   function __construct()
   {
-    # code...
+    $this->initRoutes();
+    $this->run($this->getUrl());
   }
 
   public function initRoutes(){
     $routes['home'] = array('route' => '/','controller' =>"indexController",'action' => 'index');
     $routes['contact'] = array('route' => '/contact','controller' =>"indexController",'action' => 'contact');
+
+    $this->setRoute($routes);
+  }
+
+  public function run($url){
+    array_walk($this->routes,function($route) use ($url){
+      if($url == $route['route']){
+        $class = "App\\Controllers\\".ucfirst($route['controller']);
+        $controller = new $class;
+        $action = $route['action'];
+        $controller->$action();
+      }
+    });
+  }
+
+  public function setRoute(array $routes){
+    $this->routes = $routes;
   }
 
   public function getUrl(){
